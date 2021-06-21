497151c77fd7e1483cafeb5fa61235608d1b307b
frbgd <kap17597@gmail.com>
2021-06-21 16:16:35 +0300

    README.md

--- Ask_Frbgd/settings.py
+++ Ask_Frbgd/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-g6@6q$94o&y%9gjn=9^(+#lip-r5tec!v@!vu@&e%!)k=feqg%'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
